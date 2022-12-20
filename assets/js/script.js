e930acbe82ecaf6e7eee63ace2f2e20b77fa302a
DSnoad <danialsnoad@hotmail.com>
2022-12-20 09:56:25 +0000

    Tidying up

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,25 @@ const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal")
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 document.getElementById("submit").addEventListener("click", e => postForm(e));
 
+function processOptions(form) {
+
+    let optArray = [];
+
+    for (let entry of form.entries()) {
+      if (entry[0] === "options") {
+          optArray.push(entry[1]);
+      } 
+    }
+    form.delete("options");
+
+    form.append("options", optArray.join());
+
+    return form;
+
+}
+
 async function postForm(e) {
-  const form = new FormData(document.getElementById("checksform"));
+  const form = processOptions(new FormData(document.getElementById("checksform")));
 
   const response = await fetch(API_URL, {
                           method: "POST",
@@ -21,6 +38,7 @@ async function postForm(e) {
   if (response.ok) {
     displayErrors(data);
   } else {
+    displayException(data);
     throw new Error(data.error);
   }
 }
@@ -55,7 +73,8 @@ async function getStatus(e) {
   if (response.ok) {
     displayStatus(data.expiry);
   } else {
-    throw new Error(data.error);
+      displayException(data);
+      throw new Error(data.error);
   }
 }
 
@@ -68,4 +87,19 @@ function displayStatus(data) {
   document.getElementById("results-content").innerHTML = results;
 
   resultsModal.show();
+}
+
+function displayException(data) {
+    
+  let heading = `An Exception Occurred`;
+
+  results = `<div>The API returned status code ${data.status_code}</div>`;
+  results += `<div>Error number: <strong>${data.error_no}</strong></div>`;
+  results += `<div>Error text: <strong>${data.error}</strong></div>`;
+
+  document.getElementById("resultsModalTitle").innerText = heading;
+  document.getElementById("results-content").innerHTML = results;
+
+  resultsModal.show();
+
 }
